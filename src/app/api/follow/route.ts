865ff1ea5157111c33829f58ee05589653c46cdb
danielreedy21 <dreedy@luc.edu>
2023-08-08 16:19:10 -0500

    added route for creating and removing follower relationships

---
+++ src/app/api/follow/route.ts
@@ -0,0 +1,46 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { getServerSession } from 'next-auth'
+import { prisma } from '../../../lib/prisma'
+import { authOptions } from '../auth/[...nextauth]/route'
+
+
+export async function POST(req: Request) {
+    const session = await getServerSession(authOptions)
+    const userId = session?.user.id
+    const targetUserId = await req.json();
+    // TODO: add correct error here
+    if (!userId) {
+        return
+    }
+
+    const record = await prisma.follows.create({
+        data: {
+            followerId: userId,
+            followingId: targetUserId
+        }
+    })
+
+    return NextResponse.json(record)
+}
+
+export async function DELETE(req: NextRequest) {
+    const session = await getServerSession(authOptions)
+    const userId = session?.user.id
+    const targetUserId = req.nextUrl.searchParams.get('targetUserId')
+    // TODO: return correct error here
+    if (!userId || !targetUserId) {
+        return
+    }
+
+
+    const record = await prisma.follows.delete({
+        where: {
+            followerId_followingId: {
+                followerId: userId,
+                followingId: targetUserId
+            }
+        }
+    })
+
+    return NextResponse.json(record)
+}
