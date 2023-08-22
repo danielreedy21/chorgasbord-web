0eb8c5f9c6ace0980c8f332e6770b3c58bb54911
danielreedy21 <dreedy@luc.edu>
2023-08-22 11:53:21 -0500

    changed colors to only start showing red once the chore is due

--- src/components/ChoreSquare.tsx
+++ src/components/ChoreSquare.tsx
@@ -25,23 +25,23 @@ export default function ChoreSquare( props: Props) {
 
     function getColor(percentLeft: number) {
         
-        if (percentLeft > 90.0) {
+        if (percentLeft > 80.0) {
             return "bg-gradient-0"
-        } else if (percentLeft > 80.0) {
-            return "bg-gradient-1"
-        } else if (percentLeft > 70.0) {
-            return "bg-gradient-2"
         } else if (percentLeft > 60.0) {
-            return "bg-gradient-3"
-        } else if (percentLeft > 50.0) {
-            return "bg-gradient-4"
+            return "bg-gradient-1"
         } else if (percentLeft > 40.0) {
-            return "bg-gradient-5"
-        } else if (percentLeft > 30.0) {
-            return "bg-gradient-6"
+            return "bg-gradient-2"
         } else if (percentLeft > 20.0) {
+            return "bg-gradient-3"
+        } else if (percentLeft > 0.0) {
+            return "bg-gradient-4"
+        } else if (percentLeft > -20.0) {
+            return "bg-gradient-5"
+        } else if (percentLeft > -40.0) {
+            return "bg-gradient-6"
+        } else if (percentLeft > -600.0) {
             return "bg-gradient-7"
-        } else if (percentLeft > 10.0) {
+        } else if (percentLeft > -80.0) {
             return "bg-gradient-8"
         } else {
             return "bg-gradient-9"
